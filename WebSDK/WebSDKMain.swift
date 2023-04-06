49830390f22c084789edb34af85a4df8f2352f24
Dai Tran <>
2023-04-06 16:40:43 +0700

    [OFP] parse message from the web and implement a HTTP request to get accountId

--- WebSDK/WebSDKMain.swift
+++ WebSDK/WebSDKMain.swift
@@ -8,8 +8,8 @@
 import WebKit
 import Foundation
 
-final class WebSDKMain: NSObject {
-    private let nativeToWebHandler = "jsMessageHandler"
+public final class WebSDKMain: NSObject {
+    private let nativeToWebHandler = "success"
     
     private let wkWebview: WKWebView = {
         let webview = WKWebView()
@@ -22,19 +22,60 @@ final class WebSDKMain: NSObject {
         return controller
     }()
     
-    override init() {
+    public override init() {
         super.init()
-        userContentController.add(self, name: nativeToWebHandler)
         wkWebview.configuration.userContentController = userContentController
-        
-        wkWebview.load(URLRequest(url: URL(string: "https://c.prod.verisoul.xyz/?projectId=test1234")!))
+        wkWebview.configuration.userContentController.add(self, name: nativeToWebHandler)
     }
+    
+    public func loadUrl(url: URL) {
+        DispatchQueue.main.async {
+            self.wkWebview.load(URLRequest(url: url))
+        }
+    }
+    
+    private func getAccountId(with trackingId: String, completion: ((String?) -> Void)?) {
+        let body = [
+            "trackingId": trackingId,
+            "accountId": "randomString"
+        ]
+        
+        guard
+            let url = URL(string: "https://api.prod.verisoul.xyz/predict"),
+            let bodyData = try? JSONSerialization.data(withJSONObject: body) else {
+            completion?(nil)
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("8Ldtkg35pX5YoHu2N3qq89faV6fKvakh1zk7Ps5c", forHTTPHeaderField: "x-api-key")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = bodyData
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data,
+                  let json = try? JSONSerialization.jsonObject(with: data) as? [String: String] else {
+                completion?(nil)
+                return
+            }
+            completion?(json["accountId"])
+        }.resume()
+    }
+
 }
 
 extension WebSDKMain: WKScriptMessageHandler {
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        if let body = message.body as? String, message.name == nativeToWebHandler {
-            print(body)
+    public func userContentController(_ userContentController: WKUserContentController,
+                                      didReceive message: WKScriptMessage) {
+        guard
+            let body = message.body as? [String: String],
+            let trackingId = body["trackingId"],
+            message.name == nativeToWebHandler else {
+            return
+        }
+        
+        getAccountId(with: trackingId) { accountId in
+            print(accountId)
         }
     }
 }
