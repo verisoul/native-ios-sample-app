5217c99ecede94629c5ec6d4c916900517aebd91
nketkar <niel@verisoul.xyz>
2023-11-16 11:33:08 -0600

    cleanup and update for v2

--- ZeroFakeDemo/ZeroFakeDemo.swift
+++ ZeroFakeDemo/ZeroFakeDemo.swift
@@ -2,18 +2,16 @@
 //  WebSDKMain.swift
 //  WebSDK
 //
-//  Created by Dai Tran on 06/04/2023.
-//
 
 import WebKit
 import Foundation
 
 typealias JSON = [String: Any]
 
-public final class ZeroFakeDemo: NSObject {
-    private let nativeToWebHandler = "verisoulMessageHandler"
-    private let apiKey = "yourVerisoulApiKey"
-    
+public final class Demo: NSObject {
+    private let env: String
+    private let apiKey: String
+    private let nativeToWebHandler = "verisoulHandler"
     private let wkWebview: WKWebView = {
         let webview = WKWebView()
         webview.configuration.preferences.javaScriptEnabled = true
@@ -25,13 +23,16 @@ public final class ZeroFakeDemo: NSObject {
         return controller
     }()
     
-    public override init() {
+    public init(env: String, apiKey: String) {
+        self.env = env
+        self.apiKey = apiKey
         super.init()
         wkWebview.configuration.userContentController = userContentController
         wkWebview.configuration.userContentController.add(self, name: nativeToWebHandler)
     }
     
     public func loadUrl(url: URL, from viewController: UIViewController) {
+        print("Loading webview...")
         DispatchQueue.main.async {
             viewController.view.addSubview(self.wkWebview)
             self.wkWebview.frame = .zero
@@ -42,14 +43,16 @@ public final class ZeroFakeDemo: NSObject {
     
     // Make this API call from a secure backend server NOT the client app
     // Note: this is included in the sample app for demo purposes
-    private func predict(with trackingId: String, completion: ((JSON?) -> Void)?) {
-        let body = [
-            "tracking_id": trackingId,
-            "auth_id": "internal-customer-identifier"
+    private func predict(with sessionId: String, completion: ((JSON?) -> Void)?) {
+        let body: [String: Any] = [
+            "session_id": sessionId,
+            "account": [
+                "id": "internal-customer-identifier"
+            ]
         ]
-        
+
         guard
-            let url = URL(string: "https://api.sandbox.verisoul.xyz/zerofake/predict"),
+            let url = URL(string: "https://api.\(env).verisoul.ai/session/authenticate"),
             let bodyData = try? JSONSerialization.data(withJSONObject: body) else {
             completion?(nil)
             return
@@ -69,25 +72,21 @@ public final class ZeroFakeDemo: NSObject {
             completion?(json)
         }.resume()
     }
-
 }
 
-extension ZeroFakeDemo: WKScriptMessageHandler {
+extension Demo: WKScriptMessageHandler {
     public func userContentController(_ userContentController: WKUserContentController,
                                       didReceive message: WKScriptMessage) {
-        print("Webview Response: ")
-        print(message.body)
-        
         guard
             let body = message.body as? [String: String],
             message.name.lowercased() == nativeToWebHandler.lowercased(),
-            let trackingId = body["tracking_id"]
+            let sessionId = body["session_id"]
         else {
             return
         }
         
-        predict(with: trackingId) { response in
-            print("ZeroFake API Response: ")
+        predict(with: sessionId) { response in
+            print("Verisoul API Response: ")
             print(response as Any)
         }
     }
