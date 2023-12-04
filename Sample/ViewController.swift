ac88e575f67e75bb7fb2f40851f8d2f40e6c9c35
nketkar <niel@verisoul.xyz>
2023-12-03 20:20:57 -0600

    cleanup formatting;

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -8,10 +8,8 @@ import Demo
 
 class ViewController: UIViewController {
     private let env = "sandbox" // or "prod"
-//    private let projectId = "<YOUR SANDBOX PROJECT ID>" // or prod projectId
-//    private let apiKey = "<YOUR API KEY>"
-    private let projectId = "00000000-0000-0000-0000-000000000001"
-    private let apiKey = "KxEv9FlAmC6h6L8uJxctYaAt4IjWoebAarqthqV5"
+    private let projectId = "<YOUR SANDBOX PROJECT ID>" // or prod projectId
+    private let apiKey = "<YOUR API KEY>"
     private var demo: Demo!
     
     var webViewUrl : String {
